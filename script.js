68526a40b55f204662f609d372124169e8cc10be
T3CH0X <159412906+T3CH0X@users.noreply.github.com>
2024-02-08 00:41:11 -0600

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+var map = L.map('map').setView([37.8, -96], 4);
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+}).addTo(map);
+// Add your code here to create interactive map with election poll results
